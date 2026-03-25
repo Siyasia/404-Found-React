22a25c7e1165fbdaab57dc2b48597841065337bf
Soul Ahmad <112795243+kittylugia@users.noreply.github.com>
2026-03-25 16:35:39 -0500

    avatar overhaul
    equip/unequip items
    tabs for item placement
    color selection

--- src/pages/Avatar.jsx
+++ src/pages/Avatar.jsx
@@ -1,14 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DisplayAvatar } from '../components/DisplayAvatar.jsx';
-import { useGameProfile } from '../components/useGameProfile.js';
-import { useItems } from '../components/useItems.js';
-import { useInventory } from '../components/useInventory.js';
+import { useGameProfile } from '../components/useGameProfile.jsx';
+import { useItems } from '../components/useItems.jsx';
+import { useInventory } from '../components/useInventory.jsx';
 
 export default function Avatar() {
 
     const { profile, saveProfile, loading, error } = useGameProfile();
     const { items, loading: itemLoading, error: itemError } = useItems();
     const invItems = useInventory(profile, items);
+    const [selectedCategory, setSelectedCategory] = React.useState('Base'); 
+    const COLOR_MAP = {
+        Base: {
+            1: '#BD894E',
+            2: '#D8AE73',
+            3: '#EAC487',
+            4: '#F9DCB2',
+            5: '#E5B696',
+            6: '#E9BAA6',
+            7: '#F6B889',
+            8: '#D89C70',
+            9: '#A77F61',
+            10: '#B1815E',
+            11: '#85582E',
+            12: '#976641',
+            13: '#6E513B',
+            14: '#68462D',
+            15: '#4B2F1A',
+            16: '#532A0B',
+            17: '#693E1E',
+            18: '#734E32',
+            19: '#9D96E5',
+            20: '#233F8A',
+            21: '#693AA1',
+            22: '#D87BB7',
+            23: '#7BD887',
+            24: '#40AB8E'
+        },
+        Hair: {
+            1: '#2A2828',
+            2: '#E1EAEA',
+            3: '#DB5B4C',
+            4: '#E66695',
+            5: '#CE68D1',
+            6: '#9343DD',
+            7: '#4643DD',
+            8: '#4398DD',
+            9: '#43DBDD',
+            10: '#43DD7C',
+            11: '#A8DD43',
+            12: '#F0DC71',
+            13: '#DD8642',
+            14: '#BE3F3C',
+            15: '#825929',
+            16: '#B0A56B',
+            17: '#55330A'
+        },
+        Eyebrows: null,
+        Shirts: {
+            1: '#2A2828',
+            2: '#E1EAEA',
+            3: '#DB5B4C',
+            4: '#E66695',
+            5: '#CE68D1',
+            6: '#9343DD',
+            7: '#4643DD',
+            8: '#4398DD',
+            9: '#43DBDD',
+            10: '#43DD7C',
+            11: '#A8DD43',
+            12: '#F0DC71',
+            13: '#DD8642',
+            14: '#BE3F3C',
+            15: '#55330A'
+        },
+        Eyes: {
+            1: '#2A2828',
+            2: '#E1EAEA',
+            3: '#DB5B4C',
+            4: '#E66695',
+            5: '#CE68D1',
+            6: '#9343DD',
+            7: '#4643DD',
+            8: '#4398DD',
+            9: '#43DBDD',
+            10: '#43DD7C',
+            11: '#A8DD43',
+            12: '#F0DC71',
+            13: '#DD8642',
+            14: '#BE3F3C',
+            15: '#825929',
+            16: '#55330A'
+        }
+    }
+    COLOR_MAP.Eyebrows = COLOR_MAP.Hair;
 
     if (loading || itemLoading) return <p>Loading...</p>;
 
@@ -17,7 +102,7 @@ export default function Avatar() {
         const updated = profile.inventory.map(inv => {
             //if item id matches, equip item
             if (inv.id === itemID) {
-                return { ...inv, equipped: true};
+                return { ...inv, equipped: true };
             }
             //if item id does not match but the placement does, unequip
             const item = items.find(i => i.id === inv.id);
@@ -32,12 +117,44 @@ export default function Avatar() {
 
     //unequip an item
     const unequip = (itemID) => {
-        //create updated inventory
+
+        //find item to be unequipped in inventory
+        const invItem = profile.inventory.find(i => i.id === itemID);
+        if (!invItem) return;
+
+        const item = items.find(i => i.id === invItem.id);
+        if (!item) return;
+
+        //find the default item for the placement unequipping
+        const defaultItem = items.find( i=> i.type === 'Default' && i.placement === item.placement);
+        if (!defaultItem) return;
+
+        //prevent user from unequipping a default item
+        if (invItem.id === defaultItem.id) return;
+
+        //unequip item
+        let updated = profile.inventory.map(inv => {
+            //unequip the current item
+            if (inv.id === itemID) {
+                return { ...inv, equipped: false};
+            }
+            //equip the default item with same color
+            if (inv.id === defaultItem.id) {
+                return { ...inv, equipped: true, color: inv.color ?? 1};
+            }
+            return inv;
+        });
+
+        saveProfile({ ...profile, inventory: updated });
+    };
+
+    const updateColor = (itemID, color) => {
+
         const updated = profile.inventory.map(inv =>
-            //unequip item
-            inv.id === itemID ? { ...inv, equipped: false } : inv
+            inv.id === itemID ? { ...inv, color } : inv
         );
         saveProfile({ ...profile, inventory: updated });
+
     };
 
     //used for displaying inventory by item placement
@@ -68,53 +185,85 @@ export default function Avatar() {
                 </div>
 
                 <div>
-
-                    {Object.entries(categories).map(([placement, categoryItems]) => (
-                        <div key={placement} className='card' style= {{ marginBottom: '1rem', padding: '1rem' }}>
-                            <h3 style={{ marginTop: 0 }}>
+                    <div style={{ display: 'flex', gap: '.5rem', marginBottom: '1rem' }}>
+                        {Object.keys(categories).map(placement => (
+                            <button
+                                key={placement}
+                                onClick={() => setSelectedCategory(placement)}
+                                style={{
+                                    padding: '.4rem .8rem',
+                                    backgroundColor: selectedCategory === placement ? 'gold' : '#eee',
+                                    border: '1px solid #999',
+                                    borderRadius: '4px',
+                                    cursor: 'pointer'
+                                }}
+                            >
                                 {placement.charAt(0).toUpperCase() + placement.slice(1)}
-                            </h3>
+                            </button>
+                        ))}
+                    </div>
 
-                            <div style={{
-                                display: 'flex',
-                                gap: '1rem',
-                                flexWrap: 'wrap'
-                            }}>
+                <div className='card' style={{ marginBottom: '1rem', padding: '1rem' }}>
+                    <h3 style={{ marginTop: 0 }}>
+                        {selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1)}
+                    </h3>
 
-                                {categoryItems.map(item =>
-                                    <div key={item.id} style={{
-                                        border: item.equipped ? '2px solid gold' : '1px solid #ccc',
-                                        padding: '0.5rem',
-                                        textAlign: 'center',
-                                        width: '100px'
-                                    }}>
+                    <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
+                        {categories[selectedCategory]?.map(item => {
+                            const isDefault = item.type === 'Default';
 
-                                        <img src={item.path} style={{
-                                            width: '75px',
-                                            height: '75px',
-                                            objectFit: 'contain'
-                                        }}/>
+                            return (
+                                <div key={item.id} style={{
+                                    border: item.equipped ? '2px solid gold' : '1px solid #ccc',
+                                    padding: '0.5rem',
+                                    textAlign: 'center',
+                                    width: '100px'
+                                }}>
+                                    <img src={`${item.path}.PNG`} style={{ width: '75px', height: '75px', objectFit: 'contain' }} />
 
-                                        {item.equipped ? (
-                                            <button className='btn' onClick={() => unequip(item.id)} style={{ marginTop: '0.4rem', width: '100%'}}>
+                                    {item.equipped && COLOR_MAP[item.placement] && (
+                                        <div style={{
+                                            display: 'flex',
+                                            gap: '4px',
+                                            flexWrap: 'wrap',
+                                            justifyContent: 'center',
+                                            marginTop: '0.3rem'
+                                        }}>
+                                            {Object.entries(COLOR_MAP[item.placement]).map(([color, hex]) => (
+                                                <button
+                                                    key={color}
+                                                    onClick={() => updateColor(item.id, parseInt(color))}
+                                                    style={{
+                                                        width: '16px',
+                                                        height: '16px',
+                                                        backgroundColor: hex,
+                                                        border: (item.color ?? 1) === parseInt(color) ? '2px solid black' : '1px solid #999',
+                                                        cursor: 'pointer',
+                                                        padding: '0'
+                                                    }}
+                                                />
+                                            ))}
+                                        </div>
+                                    )}
+
+                                    {item.equipped ? (
+                                        !isDefault && (
+                                            <button className='btn' onClick={() => unequip(item.id)} style={{ marginTop: '0.4rem', width: '100%' }}>
                                                 Unequip
                                             </button>
-                                        ) : (
-                                            <button className='btn' onClick={() => equip(item.id, item.placement)} style={{ marginTop: '0.4rem', width: '100%'}}>
-                                                Equip
-                                            </button>                                            
-                                        )}
-
-                                    </div>
-                                )}
-
-                            </div>
-
-                        </div>
-                    ))}
-
+                                        )
+                                    ) : (
+                                        <button className='btn' onClick={() => equip(item.id, item.placement)} style={{ marginTop: '0.4rem', width: '100%' }}>
+                                            Equip
+                                        </button>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
 
+            </div>
             </div>
 
         </section>
